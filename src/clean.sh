c1c154d49f96c916bc35b0165902adad0080e4af
shamouda <sara.salem@anu.edu.au>
2016-05-19 15:11:43 +1000

    minor

--- src/clean.sh
+++ src/clean.sh
@@ -6,8 +6,8 @@ x10/util/resilient/map/transaction/*.cc \
 x10/util/resilient/map/transaction/*.h \
 x10/util/resilient/map/partition/*.cc \
 x10/util/resilient/map/partition/*.h \
-x10/util/resilient/map/impl/migration/*.cc \
-x10/util/resilient/map/impl/migration/*.h \
+x10/util/resilient/map/migration/*.cc \
+x10/util/resilient/map/migration/*.h \
 x10/util/resilient/map/impl/*.cc \
 x10/util/resilient/map/impl/*.h \
 x10/util/resilient/map/*.cc \
